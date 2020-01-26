1cdd1b4df8653ee4bd27578ff1c961b8255c07f8
victorkeyvan <ffc150a160d37e9@gmail.com>
2020-01-26 11:31:28 +0330

    fix event Bug

--- connectors/oracleConnector.sol
+++ connectors/oracleConnector.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.8;
 
 contract Oracle {
     
-    event Log1(address sender, string _datasource, string _arg);
+    event Log1(address sender, uint timestamp, string _datasource, string _arg, uint gaslimit);
     
     
     mapping (address => uint) internal reqc;
