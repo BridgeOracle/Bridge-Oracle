080018bf759ee787282f5057cac24a8dc5a75b74
victorkeyvan <ffc150a160d37e9@gmail.com>
2020-01-26 11:11:51 +0330

    function <query1> added & change query function

--- connectors/oracleConnector.sol
+++ connectors/oracleConnector.sol
@@ -8,11 +8,15 @@ contract Oracle {
     
     mapping (address => uint) internal reqc;
 	
-	 function query(string calldata _datasource, string calldata _arg) external payable returns(bytes32 _id) {
-	  reqc[msg.sender]++;
-	  bytes32 customHash = keccak256('keyvan');
-	  emit Log1(msg.sender, _datasource, _arg);
-	  return customHash;
+	function query(string calldata _datasource, string calldata _arg) external payable returns(bytes32 _id) {
+		//set gasLimit tron blockchain
+	 	return query1(0, _datasource, _arg, 200000);
     }
-    
+
+    function query1(uint _timestamp, string memory _datasource, string memory _arg, uint _gaslimit) public payable returns(bytes32 _id) {
+    	reqc[msg.sender]++;
+	  	bytes32 customHash = keccak256('keyvan');
+	  	emit Log1(msg.sender, _datasource, _arg);
+	  	return customHash;
+    }
 }
