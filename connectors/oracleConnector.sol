35ac4633a7cd24c30206b08578df92c7fbbb33af
victorkeyvan <ffc150a160d37e9@gmail.com>
2020-01-26 11:23:51 +0330

    update Event log 'Log1'

--- connectors/oracleConnector.sol
+++ connectors/oracleConnector.sol
@@ -13,10 +13,11 @@ contract Oracle {
 	 	return query1(0, _datasource, _arg, 200000);
     }
 
+
     function query1(uint _timestamp, string memory _datasource, string memory _arg, uint _gaslimit) public payable returns(bytes32 _id) {
     	reqc[msg.sender]++;
 	  	bytes32 customHash = keccak256('keyvan');
-	  	emit Log1(msg.sender, _datasource, _arg);
+	  	emit Log1(msg.sender, _timestamp, _datasource, _arg, _gaslimit);
 	  	return customHash;
     }
 }
