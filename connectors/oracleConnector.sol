7a63343657284d58a0990bdde5a26a8bcbed326a
victorkeyvan <ffc150a160d37e9@gmail.com>
2020-01-26 16:56:26 +0330

    function <query2> & event 'Log2' added

--- connectors/oracleConnector.sol
+++ connectors/oracleConnector.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.5.8;
 
 contract Oracle {
     
-    event Log1(address sender, uint timestamp, string _datasource, string _arg, uint gaslimit);
-    
+    event Log1(address sender, bytes32 cid, uint timestamp, string _datasource, string _arg, uint gaslimit);
+    event Log2(address sender, bytes32 cid, uint timestamp, string _datasource, string _arg, string _arg2, uint gaslimit);
     
     mapping (address => uint) internal reqc;
 	
@@ -13,15 +13,29 @@ contract Oracle {
 	 	return query1(0, _datasource, _arg, 200000);
     }
 
-    function query_withGasLimit(uint _timestamp, string memory _datasource, string memory _arg, uint _gaslimit) external payable returns(bytes32 _id) {
+    function query(uint _timestamp, string calldata _datasource, string calldata _arg) payable external returns(bytes32 _id) {
+    	return query1(_timestamp, _datasource, _arg, 200000);
+    }
+
+    function query_withGasLimit(uint _timestamp, string calldata _datasource, string calldata _arg, uint _gaslimit) external payable returns(bytes32 _id) {
     	return query1(_timestamp, _datasource, _arg, _gaslimit);
     }
 
+    function query2(uint _timestamp, string calldata _datasource, string calldata _arg1, string calldata _arg2) external payable returns(bytes32 _id) {
+    	return query2(_timestamp, _datasource, _arg1, _arg2, 200000);
+    }
 
     function query1(uint _timestamp, string memory _datasource, string memory _arg, uint _gaslimit) public payable returns(bytes32 _id) {
     	reqc[msg.sender]++;
 	  	bytes32 customHash = keccak256('keyvan');
-	  	emit Log1(msg.sender, _timestamp, _datasource, _arg, _gaslimit);
+	  	emit Log1(msg.sender, customHash, _timestamp, _datasource, _arg, _gaslimit);
+	  	return customHash;
+    }
+
+    function query2(uint _timestamp, string memory _datasource, string memory _arg1, string memory _arg2, uint _gasLimit) public payable returns(bytes32 _id) {
+    	reqc[msg.sender]++;
+	  	bytes32 customHash = keccak256('keyvan');
+	  	emit Log2(msg.sender, customHash, _timestamp, _datasource, _arg1, _arg2, _gaslimit);
 	  	return customHash;
     }
 }
