403fcb70c774a66bccb74031a8f682221b0ca6e2
victorkeyvan <ffc150a160d37e9@gmail.com>
2020-01-27 11:26:21 +0330

    function <query2_withGasLimit> added

--- connectors/oracleConnector.sol
+++ connectors/oracleConnector.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.5.8;
 contract Oracle {
     
     event Log1(address sender, bytes32 cid, uint timestamp, string _datasource, string _arg, uint gaslimit);
-    event Log2(address sender, bytes32 cid, uint timestamp, string _datasource, string _arg, string _arg2, uint gaslimit);
+    event Log2(address sender, bytes32 cid, uint timestamp, string _datasource, string _arg1, string _arg2, uint gaslimit);
     
     mapping (address => uint) internal reqc;
 	
@@ -24,6 +24,10 @@ contract Oracle {
     function query2(uint _timestamp, string calldata _datasource, string calldata _arg1, string calldata _arg2) external payable returns(bytes32 _id) {
     	return query2(_timestamp, _datasource, _arg1, _arg2, 200000);
     }
+    
+    function query2_withGasLimit(uint _timestamp, string calldata _datasource, string calldata _arg1, string calldata _arg2, uint _gasLimit) external payable returns(bytes32 _id) {
+    	return query2(_timestamp, _datasource, _arg1, _arg2, _gasLimit);
+    }
 
     function query1(uint _timestamp, string memory _datasource, string memory _arg, uint _gaslimit) public payable returns(bytes32 _id) {
     	reqc[msg.sender]++;
@@ -38,4 +42,5 @@ contract Oracle {
 	  	emit Log2(msg.sender, customHash, _timestamp, _datasource, _arg1, _arg2, _gasLimit);
 	  	return customHash;
     }
+
 }
