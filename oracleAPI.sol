6c0787c6e394cece41110e516447d30a2e825402
_keyvan <nessus1373@gmail.com>
2020-02-01 15:00:51 +0330

    function <strCompare> added

--- oracleAPI.sol
+++ oracleAPI.sol
@@ -122,4 +122,27 @@ contract oracle {
 		
 	}
 
+	function strCompare(string memory _a, string memory _b) internal pure returns (int _returnCode) {
+        bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        uint minLength = a.length;
+        if (b.length < minLength) {
+            minLength = b.length;
+        }
+        for (uint i = 0; i < minLength; i ++) {
+            if (a[i] < b[i]) {
+                return -1;
+            } else if (a[i] > b[i]) {
+                return 1;
+            }
+        }
+        if (a.length < b.length) {
+            return -1;
+        } else if (a.length > b.length) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
 }
