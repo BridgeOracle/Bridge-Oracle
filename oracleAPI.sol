072b3fa77395969be7045ca089cfc1da3aae0d25
_keyvan <nessus1373@gmail.com>
2020-02-01 15:04:49 +0330

    function <indexOf> added

--- oracleAPI.sol
+++ oracleAPI.sol
@@ -145,4 +145,28 @@ contract oracle {
         }
     }
 
+	function indexOf(string memory _haystack, string memory _needle) internal pure returns (int _returnCode) {
+        bytes memory h = bytes(_haystack);
+        bytes memory n = bytes(_needle);
+        if (h.length < 1 || n.length < 1 || (n.length > h.length)) {
+            return -1;
+        } else if (h.length > (2 ** 128 - 1)) {
+            return -1;
+        } else {
+            uint subindex = 0;
+            for (uint i = 0; i < h.length; i++) {
+                if (h[i] == n[0]) {
+                    subindex = 1;
+                    while(subindex < n.length && (i + subindex) < h.length && h[i + subindex] == n[subindex]) {
+                        subindex++;
+                    }
+                    if (subindex == n.length) {
+                        return int(i);
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
 }
