6431bf3c572f955b4a89674a0610b2a5ac2dfa02
_keyvan <nessus1373@gmail.com>
2020-02-01 15:15:38 +0330

    function <safeParseInt> added

--- oracleAPI.sol
+++ oracleAPI.sol
@@ -208,5 +208,33 @@ contract oracle {
         }
         return string(babcde);
     }
+	
+	function safeParseInt(string memory _a) internal pure returns (uint _parsedInt) {
+        return safeParseInt(_a, 0);
+    }
 
+    function safeParseInt(string memory _a, uint _b) internal pure returns (uint _parsedInt) {
+        bytes memory bresult = bytes(_a);
+        uint mint = 0;
+        bool decimals = false;
+        for (uint i = 0; i < bresult.length; i++) {
+            if ((uint(uint8(bresult[i])) >= 48) && (uint(uint8(bresult[i])) <= 57)) {
+                if (decimals) {
+                   if (_b == 0) break;
+                    else _b--;
+                }
+                mint *= 10;
+                mint += uint(uint8(bresult[i])) - 48;
+            } else if (uint(uint8(bresult[i])) == 46) {
+                require(!decimals, 'More than one decimal encountered in string!');
+                decimals = true;
+            } else {
+                revert("Non-numeral character encountered in string!");
+            }
+        }
+        if (_b > 0) {
+            mint *= 10 ** _b;
+        }
+        return mint;
+    }
 }
