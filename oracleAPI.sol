ed2a7e82112cbd4cf29167dab73cf3d24c9499ee
_keyvan <nessus1373@gmail.com>
2020-02-01 15:20:18 +0330

    function <parseInt> added

--- oracleAPI.sol
+++ oracleAPI.sol
@@ -237,4 +237,33 @@ contract oracle {
         }
         return mint;
     }
+	
+	function parseInt(string memory _a) internal pure returns (uint _parsedInt) {
+        return parseInt(_a, 0);
+    }
+
+    function parseInt(string memory _a, uint _b) internal pure returns (uint _parsedInt) {
+        bytes memory bresult = bytes(_a);
+        uint mint = 0;
+        bool decimals = false;
+        for (uint i = 0; i < bresult.length; i++) {
+            if ((uint(uint8(bresult[i])) >= 48) && (uint(uint8(bresult[i])) <= 57)) {
+                if (decimals) {
+                   if (_b == 0) {
+                       break;
+                   } else {
+                       _b--;
+                   }
+                }
+                mint *= 10;
+                mint += uint(uint8(bresult[i])) - 48;
+            } else if (uint(uint8(bresult[i])) == 46) {
+                decimals = true;
+            }
+        }
+        if (_b > 0) {
+            mint *= 10 ** _b;
+        }
+        return mint;
+    }
 }
