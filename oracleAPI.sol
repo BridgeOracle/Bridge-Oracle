a023c314f88b107b3744d6ac1e6a257dc7ee8d11
keyvanA <ffc150a160d37e9@gmail.com>
2020-07-08 11:43:55 +0430

    cbor license was added

--- oracleAPI.sol
+++ oracleAPI.sol
@@ -20,6 +20,39 @@ contract oracleI {
     function getTokenPrice() public returns(uint256 _price);
 }
 
+
+/*
+
+
+Begin solidity-cborutils
+
+https://github.com/smartcontractkit/solidity-cborutils
+
+MIT License
+
+Copyright (c) 2018 SmartContract ChainLink, Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+*/
+
 library Buffer {
 
     struct buffer {
@@ -205,6 +238,12 @@ library CBOR {
         encodeIndefiniteLengthType(_buf, MAJOR_TYPE_CONTENT_FREE);
     }
 }
+
+/*
+
+End solidity-cborutils
+
+*/
 
 contract OracleAddrResolverI {
     function getAddress(string memory ot) public returns(address _address);
